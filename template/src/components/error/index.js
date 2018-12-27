51540341f1b66fe4893827f98c80c38eb35f3638
justin lee <yuminjustin@163.com>
2018-12-27 16:26:05 +0800

    Update index.js

--- template/src/components/error/index.js
+++ template/src/components/error/index.js
@@ -1,5 +1,8 @@
+var style = require('B/assets/css/page.css');
 module.exports ={
     view: () => {
-        return m("div.error_404", [m('h1','404'),m('p', 'Ooops!')])
+        return m("div", {
+            "class": style.error_404
+        },[m('h1','404'),m('p', 'Ooops!')])
     }
 }
